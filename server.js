fbab24db3ca0c39bb478e74212eed65c1d1ba633
gitgut0110 <jsimms0110@gmail.com>
2018-08-23 17:06:25 +0200

    end of class

--- server.js
+++ server.js
@@ -1,83 +1,72 @@
-const express = require("express");
-const app = express();
-// const Cat\ = require("./models/Cat");
-const Movie = require("./models/Movie")
-//MONGOOSE
-const mongoose = require("mongoose");
+const express = require('express')
+const mongoose = require('mongoose')
+const Movie = require('./models/Movie')
+const path = require('path')
+const bodyParser = require('body-parser')
+const moment = require('moment')
 
-const hbs = require("hbs");
-const path = require("path");
+mongoose
+    .connect(
+        'mongodb://localhost/video',
+        { useNewUrlParser: true }
+    )
+    .then(() => {
+        console.log('connected to DB')
+    })
 
-//MONGOOSE REMOVE DEPRECATION WARNING ----------------------
-mongoose.connect(
-  "mongodb://localhost/video",
-  { useNewUrlParser: true }
-);
+const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
-// app.use(express.static(path.join(__dirname, "./public")));
-// hbs.registerPartials(path.join(__dirname, "./views/partials"));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get("/", (req, res, next) => {
-  res.render("index", { title: "HOMEPAGE" });
-});
+app.use((req, res, next) => {
+    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} - ${req.method} - ${req.url}`)
+    next()
+})
 
+app.get('/search', (req, res) => {
+    res.render('form')
+})
 
 app.get('/movie', (req, res) => {
-  const {
-      title, rate, year,
-  } = req.query
+    const {
+        title, rate, year,
+    } = req.query
 
-  const query = {
-      $and: [
-      title ? { title : {$regex: new RegExp('^'+title+ '.*','i')} } : {},
-          rate ? { rate } : {},
-          year ? { year } : {},
-      ],
-  }
+    const query = {
+        $and: [
+            title ? { title: { $regex: new RegExp('^' + title + '.*', 'i') } } : {},
+            rate ? { rate } : {},
+            year ? { year } : {},
+        ],
+    }
 
-  console.log(query['$and'])
-
-  Movie.find(query).then(movies => res.send(movies))
+    Movie.find(query).then(movies => res.send(movies))
 })
-// app.get("/cat", (req, res) => {
-//   console.log("QUERY", req.query)
-//   const { age,name} = req.query
-//   Cat.find({age, name })
-//   .then(cats => res.send(cats))
-// });
 
+app.post('/movie', (req, res) => {
+    const {
+        title, rate, year,
+    } = req.body
 
-// const myCat = new Cat({
-//   name: "Minou",
-//   age: 13,
-//   fur_color: "white",
-//   hobbies: ["eating", "sleeping", "tree climbing"]
-// });
-// SAVE THE INSTANCE-----------------------------------
-// console.log(myCat)
+    const query = {
+        $and: [
+            title ? { title: { $regex: new RegExp('^' + title + '.*', 'i') } } : {},
+            rate ? { rate } : {},
+            year ? { year } : {},
+        ],
+    }
 
-//
+    Movie.find(query).then(movies => res.send(movies))
+})
 
-// app.get("/cat/:catId", (req, res) => {
-//   res.send(req.params);
-// });
+app.listen(3000)
 
-//RETURN ALL CATS WITH MATCHING NAME -----------------------------
-// app.get("/cat/:catName", (req, res) => {
-//   Cat.find({name: req.params.catName})
-//   .then(cats => res.send(cats))
-// });
-
-// app.get("/cat", (req, res) => {
-//   console.log("QUERY", req.query)
-//   Cat.find({age: req.query.age})
-//   .then(cats => res.send(cats))
-// });
-
-
-
-app.listen(3000);
-
+process.on('SIGINT', () => {
+    mongoose.connection.close().then(() => {
+        console.log('CLOSED DB CONNECTION')
+        process.exit(0)
+    })
+})
